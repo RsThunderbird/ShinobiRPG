12f46cb12cc7777051a92314694996f8af053632
RsThunderbird <90311157+RsThunderbird@users.noreply.github.com>
2025-12-30 20:11:59 +0530

    Delete website directory

--- website/js/util/dom-helper.js
+++
@@ -1,8 +0,0 @@
-define({
-  getWidthPropertyFromComputedStyle: function(compStyle, propName) {
-    var res, tmp;
-    tmp = compStyle.getPropertyValue(propName);
-    res = parseInt(tmp.substr(0, tmp.indexOf("px")));
-    return res;
-  }
-});
